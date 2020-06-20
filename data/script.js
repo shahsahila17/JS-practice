12bf9a9639b053f906ae7ad6ab1914bc9d4dd92d
shahsahila17 <shahsahila17@gmail.com>
2020-06-20 17:52:25 +0900

    My first js page

---
+++ data/script.js
@@ -0,0 +1,10 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for(var name in names){
+    var firstLetter =names[name].charAt(0);
+    if(firstLetter==="J" || firstLetter==="j"){
+        byeSpeaker.speak(names[name]);
+    }
+    else{
+        helloSpeaker.speak(names[name]);
+    }
+}
